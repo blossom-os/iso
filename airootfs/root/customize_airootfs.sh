72046dd935e480df23cb4bb35340b37980746038
koyu <me@koyu.space>
2025-11-14 05:41:04 +0100

    Remove workflow_dispatch input for upload-to-r2 and simplify upload conditions in build-arch-iso.yml; add bun install command in customize_airootfs.sh for installer setup

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -23,6 +23,7 @@ sudo -u liveuser bash -c 'curl -fsSL https://bun.sh/install | bash'
 cd /opt
 git clone https://github.com/blossom-os/installer.git blossomos-installer
 chown -R liveuser:liveuser /opt/blossomos-installer
+sudo -u liveuser bash -c 'cd /opt/blossomos-installer && bun install'
 
 # Install themes and icons
 wget -O /tmp/Bibata_Cursor.tar.gz https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Classic.tar.xz
